8b42b68bdae9d3fd7a9852801375e181ebb2a755
kevin5naug <kevinguan_37@outlook.com>
2018-07-17 17:29:00 +0800

    test begins

--- genetic_algorithm/train_GA.py
+++ genetic_algorithm/train_GA.py
@@ -4,6 +4,8 @@ import gym
 import scipy.signal
 import os
 import time
+import logz
+import inspect
 
 import torch
 from torch.autograd import Variable
@@ -16,6 +18,7 @@ from torch.distributions import Categorical
 from torch.distributions.multivariate_normal import MultivariateNormal
 import random
 import operator
+
 class Member_discrete(nn.Module):
     def __init__(self, inputdim, outputdim, n_layers, hiddendim, activation, output_activation):
         super(Member_discrete, self).__init__()
@@ -195,14 +198,38 @@ def get_init_population(n, input_size, output_size, n_layers, size, activation, 
         member_list.append(build_mlp(input_size, output_size, n_layers, size, activation, output_activation, discrete))
     return member_list
 
-def train_ga(env_name='HalfCheetah-v2', n_exp=1, prob_save=0.05, n_gen=100, gamma=0.5, sigma=0.05, pop_size=50, fitness_eval_episodes=40, max_steps=150, n_elite=20, seed=1, n_layers=1, size=32, network_activation='tanh', output_activation=None):
+def train_ga(exp_name='',
+        env_name='HalfCheetah-v2',
+        logdir=None,
+        prob_save=0.05, 
+        n_gen=100, 
+        gamma=0.5, 
+        sigma=1e-3, 
+        pop_size=100, 
+        fitness_eval_episodes=40, 
+        max_steps=150, 
+        n_elite=20, 
+        seed=1, 
+        n_layers=2, 
+        size=64, 
+        network_activation='leaky_relu', 
+        output_activation='tanh'):
+    
+
+    start=time.time()
+    logz.configure_output_dir(logdir)
+    args=inspect.getargspec(train_ga)[0]
+    locals_ = locals()
+    params = {k: locals_[k] if k in locals_ else None for k in args}
+    logz.save_params(params)
+
     torch.manual_seed(seed)
     np.random.seed(seed)
     env=gym.make(env_name)
     env.seed(seed)
 
     discrete=isinstance(env.action_space, gym.spaces.Discrete)
-    max_steps=max_steps or env.spec.max_episode_steps
+    max_steps=int(max_steps or env.spec.max_episode_steps)
 
     input_size=env.observation_space.shape[0]
     output_size=env.action_space.n if discrete else env.action_space.shape[0]
@@ -224,59 +251,109 @@ def train_ga(env_name='HalfCheetah-v2', n_exp=1, prob_save=0.05, n_gen=100, gamm
         output_a=None
 
     center_return_all=[]
-    for i_experiment in range(n_exp):
-        member_archive=Archive(prob_save)
-        population=get_init_population(pop_size, input_size, output_size, n_layers, size, activation, output_a, discrete)
-        
-        for member in population:
-            member.setScore(compute_fitness(env, member, member_archive, fitness_eval_episodes, gamma, max_steps, discrete))
-        
+    member_archive=Archive(prob_save)
+    population=get_init_population(pop_size, input_size, output_size, n_layers, size, activation, output_a, discrete)
+    
+    for member in population:
+        member.setScore(compute_fitness(env, member, member_archive, fitness_eval_episodes, gamma, max_steps, discrete))
+    
+    sort_members_in_place(population, reverse=True)
+    
+    #save in archive
+    for member in population:
+        member_archive.save(member)
+    
+    population=population[:n_elite]
+    center_return_list=[]
+    current_best_fitness_score=float(population[0].score)
+    current_best_reward_score=float(population[0].reward_score)
+    center_return_list.append(current_best_reward_score)
+    for i_gen in range(n_gen):
+        offsprings=[]
+
+        for i in range(int((pop_size-n_elite)/2)):
+            parent_index=random.randint(0,n_elite-1)
+            parent=population[parent_index]
+            offspring1, offspring2=perturb_member(parent, sigma, input_size, output_size, n_layers, size, activation, output_a, discrete)
+            offsprings.append(offspring1)
+            offsprings.append(offspring2)
+
+        for member in offsprings:
+            member.setScore((compute_fitness(env, member, member_archive, fitness_eval_episodes, gamma, max_steps, discrete)))
+
+        population=population+offsprings
         sort_members_in_place(population, reverse=True)
-        
-        #save in archive
-        for member in population:
+        for member in offsprings:
             member_archive.save(member)
-        
         population=population[:n_elite]
-        center_return_list=[]
         current_best_fitness_score=float(population[0].score)
         current_best_reward_score=float(population[0].reward_score)
-        print('generation 0 current best', current_best_fitness_score, current_best_reward_score)
+        logz.log_tabular("Time", time.time()-start)
+        logz.log_tabular("Iteration", i_gen)
+        logz.log_tabular("AverageFitness", current_best_fitness_score)
+        logz.log_tabular("stdFitness", -1)
+        logz.log_tabular("AverageReturn", current_best_reward_score)
+        logz.log_tabular("stdReturn", -1)
+        logz.log_tabular("dontcare1", -1)
+        logz.log_tabular("dontcare2", -1)
+        logz.log_tabular("dontcare3", -1)
+        logz.log_tabular("dontcare4", -1)
+        logz.dump_tabular()
+        logz.pickle_tf_vars()
         center_return_list.append(current_best_reward_score)
-        for i_gen in range(n_gen):
-            offsprings=[]
 
-            for i in range(int((pop_size-n_elite)/2)):
-                parent_index=random.randint(0,n_elite-1)
-                parent=population[parent_index]
-                offspring1, offspring2=perturb_member(parent, sigma, input_size, output_size, n_layers, size, activation, output_a, discrete)
-                offsprings.append(offspring1)
-                offsprings.append(offspring2)
+    center_return_all.append(center_return_list)
+    env.close()
 
-            for member in offsprings:
-                member.setScore((compute_fitness(env, member, member_archive, fitness_eval_episodes, gamma, max_steps, discrete)))
+def main():
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('env_name', type=str, default='CartPole-v0')
+    parser.add_argument('--exp_name', type=str, default='ga_ns')
+    parser.add_argument('--n_experiments', '-e', type=int, default=1)
+    parser.add_argument('--prob_save', type=float, default=0.05)
+    parser.add_argument('--n_generations', type=int, default=100)
+    parser.add_argument('--gamma', type=float, default=5e-6)
+    parser.add_argument('--sigma', type=float, default=5e-4)
+    parser.add_argument('--pop_size', type=int, default=100)
+    parser.add_argument('--fitness_eval_episodes', type=int, default=30)
+    parser.add_argument('--max_steps', type=float, default=-1.)
+    parser.add_argument('--n_elite', type=int, default=20)
+    parser.add_argument('--seed', type=int, default=1)
+    parser.add_argument('--n_layers', '-l', type=int, default=2)
+    parser.add_argument('--size', '-s', type=int, default=512)
+    parser.add_argument('--activation', '-a', type=str, default='leaky_relu')
+    parser.add_argument('--output_activation', '-oa', type=str, default='None')
+    args = parser.parse_args()
+    
+    if not(os.path.exists('data')):
+        os.makedirs('data')
+    logdir = args.exp_name + '_' + args.env_name + '_' + time.strftime("%d-%m-%Y_%H-%M-%S")
+    logdir = os.path.join('data', logdir)
+    if not(os.path.exists(logdir)):
+        os.makedirs(logdir)
+    
+    max_path_length = args.max_steps if args.max_steps > 0 else None
+    
+    for e in range(args.n_experiments):
+        seed=args.seed+10*e
+        print('Running experiment with seed %d'%seed)
+        train_ga(exp_name=args.exp_name,
+                env_name=args.env_name,
+                logdir=os.path.join(logdir, '%d'%seed),
+                prob_save=args.prob_save,
+                n_gen=args.n_generations,
+                gamma=args.gamma,
+                sigma=args.sigma,
+                pop_size=args.pop_size,
+                fitness_eval_episodes=args.fitness_eval_episodes,
+                max_steps=max_path_length,
+                n_elite=args.n_elite,
+                seed=args.seed,
+                n_layers=args.n_layers,
+                size=args.size,
+                network_activation=args.activation,
+                output_activation=args.output_activation)
 
-            population=population+offsprings
-            sort_members_in_place(population, reverse=True)
-            for member in offsprings:
-                member_archive.save(member)
-            population=population[:n_elite]
-            current_best_fitness_score=float(population[0].score)
-            current_best_reward_score=float(population[0].reward_score)
-            print('generation', i_gen+1, 'current best', current_best_fitness_score, current_best_reward_score)
-            center_return_list.append(current_best_reward_score)
-
-        center_return_all.append(center_return_list)
-
-        import seaborn as sns
-        import matplotlib.pyplot as plt
-        for i in range(n_exp):
-            ax=sns.tsplot(data=np.array(center_return_all), color='blue')
-        plt.xlabel('Epoch')
-        plt.ylabel('Return')
-        plt.tight_layout()
-        plt.show()
-
-train_ga()
-
-
+if __name__ == "__main__":
+    main()
